36271222f2f99c7b68cac4faee7fc11c32c50a5a
AXIS5hacker <AXIS5_secret@163.com>
2023-05-14 22:29:27 +0800

    updated version info

--- Dynamix chart width control/main.cpp
+++ Dynamix chart width control/main.cpp
@@ -11,7 +11,7 @@ struct stat st;
 
 int main(int argc, char* argv[])
 {
-	cout << "Dynamix Chart Width Changer v0.7.6" << endl;
+	cout << "Dynamix Chart Width Changer v0.7.9" << endl;
 	cout << "Created by AXIS5" << endl;
 	cout << "Special thanks: i0ntempest" << endl << endl << endl;
 	char pbuf[260];
