8b7d8cdbef3c450d299ddd0c3340c75b00e36915
AXIS5hacker <AXIS5_secret@163.com>
2023-08-09 22:26:37 +0800

    update some translations

--- Dynamix chart width control GUI/DNX_widthGUI_zh_CN.ts
+++ Dynamix chart width control GUI/DNX_widthGUI_zh_CN.ts
@@ -5,124 +5,150 @@
     <name>MainGUI</name>
     <message>
         <location filename="maingui.ui" line="32"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="293"/>
         <source>Dynamix note width changer GUI</source>
         <translation>Dynamix自制谱键宽缩放器</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="49"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="298"/>
         <source>Load Chart</source>
         <translation>加载谱面</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="62"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="299"/>
         <source>Edit queue:</source>
         <translation>编辑队列:</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="83"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="300"/>
         <source>SAVE</source>
         <translation>保存谱面</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="125"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="302"/>
         <source>Input File</source>
         <translation>输入谱面</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="141"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="303"/>
         <source>Width multiplier</source>
         <translation>缩放倍率</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="203"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="304"/>
         <source>0.01</source>
         <translation></translation>
     </message>
     <message>
         <location filename="maingui.ui" line="216"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="305"/>
         <source>5.00</source>
         <translation></translation>
     </message>
     <message>
         <location filename="maingui.ui" line="233"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="306"/>
         <source>Time</source>
         <translation>时间控制</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="245"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="307"/>
         <source>Start time(bar):</source>
         <translation>开始时间(小节):</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="258"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="308"/>
         <source>End time(bar):</source>
         <translation>结束时间(小节):</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="324"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="309"/>
         <source>Set Start Time</source>
         <translation>指定开始时间</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="340"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="310"/>
         <source>Set EndTime</source>
         <translation>指定结束时间</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="396"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="311"/>
         <source>EXIT</source>
         <translation>退出</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="412"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="312"/>
         <source>Sides</source>
         <translation>指定下落面</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="424"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="313"/>
         <source>Middle</source>
         <translation>正面</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="440"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="314"/>
         <source>Left</source>
         <translation>左面</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="456"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="315"/>
         <source>Right</source>
         <translation>右面</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="473"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="316"/>
         <source>Browse</source>
         <translation>浏览</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="494"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="317"/>
         <source>APPLY CHANGE</source>
         <translation>应用键宽缩放</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="671"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="319"/>
         <source>Dynamix Note Width Changer</source>
         <oldsource>Dynamix Note width changer</oldsource>
         <translation>Dynamix自制谱键宽缩放器</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="689"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="320"/>
         <source>languages</source>
         <translation>语言</translation>
     </message>
     <message>
         <location filename="maingui.ui" line="699"/>
         <location filename="maingui.ui" line="709"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="294"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="296"/>
         <source>English</source>
         <translation></translation>
     </message>
     <message>
         <location filename="maingui.ui" line="704"/>
         <location filename="maingui.ui" line="714"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="295"/>
+        <location filename="out/build/dnx_width_change_autogen/include/ui_maingui.h" line="297"/>
         <source>中文</source>
         <translation></translation>
     </message>
